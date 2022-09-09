49a1264c83d5c25574faee62db5170d9d80c54d4
Jonatan Cantero Atienza <jonatan.cantero@botonit.com>
2022-09-09 10:04:55 +0200

    ThirdGit

--- src/main/java/com/example/bosonit54/Controller/Controler.java
+++ src/main/java/com/example/bosonit54/Controller/Controler.java
@@ -3,18 +3,19 @@ package com.example.bosonit54.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-
+@PropertySource("classpath:application.yml")
 @Component
 public class Controler {
 
     @Value("${greeting}")
     private String message1;
+
     @Value("${my.number}")
     private String message2;
-
     @Value("${new.property:new.property no tiene valor}")
     private String message3;
 
